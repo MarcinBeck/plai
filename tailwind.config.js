7327fe03bdc746a30b2cfd3b08bd3c1ddf312e35
MarcinBeck <marcin.beck@gmail.com>
2025-11-19 13:37:44 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Wskazuje, gdzie szukać klas Tailwind (jest to krytyczne dla Next.js App Router)
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Obsługuje nasz plik app/page.jsx
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // Przyda się w przyszłości, gdy dodamy komponenty
+  ],
+  theme: {
+    extend: {}, // Tutaj można dodawać niestandardowe kolory, fonty itp.
+  },
+  plugins: [], // Tutaj można dodawać pluginy Tailwind, np. @tailwindcss/typography
+};
