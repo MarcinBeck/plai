f8b82e3615618476c302d33b6239fd9e428f1a01
MarcinBeck <marcin.beck@gmail.com>
2025-11-19 13:53:36 +0100

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    // Tailwind CSS jest ładowany jako pierwszy plugin
-    tailwindcss: {},
-    // Autoprefixer dodaje prefiksy specyficzne dla przeglądarek do CSS
-    autoprefixer: {},
-  },
-};
